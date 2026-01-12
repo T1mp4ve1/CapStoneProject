15d884cc2f0d83160de547df58f263d691c63e1c
T1mp4ve1 <pavelwire@outlook.it>
2026-01-12 16:34:15 +0100

    dipendenze

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './App.css'
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="d-flex justify-content-center">
+        <button className='btn btn-outline-primary' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
