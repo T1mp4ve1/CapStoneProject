4d19b083455b45634e962ead95d24ed530b36270
T1mp4ve1 <pavelwire@outlook.it>
2026-01-30 22:25:12 +0100

    read orders front

--- frontend/src/components/AdminSections/OrdersManager.jsx
+++ frontend/src/components/AdminSections/OrdersManager.jsx
@@ -1,9 +1,96 @@
+import { useEffect, useState } from "react";
+import { getOrders } from "../../services/ordersService";
+
 function OrdersManager() {
-  
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //C
+
+  // R
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const data = await getOrders();
+        setOrders(data.data);
+        console.log(data.data);
+      } catch (err) {
+        console.error("Guitar page fetch error:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchOrders();
+  }, []);
+
+  //U
+
+  //D
+
+  if (loading) {
+    return (
+      <div className="spinner-border flexContainerCenter" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="container80 manageContaner">
         <h2>Ordini</h2>
+
+        <table className="table table-striped table-hover mt-4">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Data</th>
+              <th>Indirizzo</th>
+              <th>Totale</th>
+              <th>Stato</th>
+              <th>Prodotti</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {orders &&
+              orders.map((o) => (
+                <>
+                  <tr className="table-primary" key={o.id}>
+                    <td>{o.id}</td>
+                    <td>{new Date(o.createdAt).toLocaleString()}</td>
+                    <td>{o.address}</td>
+                    <td>{o.total} €</td>
+                    <td>{o.state}</td>
+                    <td>{o.products.length}</td>
+                  </tr>
+
+                  <tr>
+                    <td colSpan="6">
+                      <table className="table table-sm table-bordered mt-2">
+                        <thead>
+                          <tr>
+                            <th>Prodotto</th>
+                            <th>Quantità</th>
+                            <th>Prezzo unitario</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {o.products.map((p) => (
+                            <tr key={p.id}>
+                              <td>{p.productName}</td>
+                              <td>{p.quantity}</td>
+                              <td>{p.unitPrice} €</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </>
+              ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
---
+++ frontend/src/services/ordersService.js
@@ -0,0 +1,101 @@
+import { getToken } from "./authService";
+
+const api = import.meta.env.VITE_API_URL;
+
+// C
+// export const createGuitar = async (guitar) => {
+//   try {
+//     const token = getToken();
+//     const res = await fetch(`${api}/Guitar`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify(guitar),
+//     });
+//     if (!res.ok) {
+//       throw new Error(`Error create! Status: ${res.status}`);
+//     }
+//     return await res.json();
+//   } catch (err) {
+//     console.error("Create guitar error", err);
+//     throw err;
+//   }
+// };
+
+// R
+export const getOrders = async () => {
+  try {
+    const res = await fetch(`${api}/Order`);
+    if (!res.ok) {
+      throw new Error(`Error read! Status: ${res.status}`);
+    }
+    return await res.json();
+  } catch (err) {
+    console.error("Read orders error:", err);
+    throw err;
+  }
+};
+
+// export const getGuitar = async (id) => {
+//   try {
+//     const guitarRes = await fetch(`${api}/Guitar/${id}`);
+//     if (!guitarRes.ok) {
+//       throw new Error(`Error guitar! Status: ${guitar.status}`);
+//     }
+//     const guitar = await guitarRes.json();
+
+//     const imgsRes = await fetch(`${api}/Image/${id}`);
+//     if (!imgsRes.ok) {
+//       throw new Error(`Error imgs! Status: ${imgs.status}`);
+//     }
+//     const imgs = await imgsRes.json();
+
+//     return { ...guitar, imgs, mainImg: imgs.find((i) => i.isMain)?.url };
+//   } catch (err) {
+//     console.error("Read guitar error:", err);
+//     throw err;
+//   }
+// };
+
+// U
+// export const updateGuitar = async (id, guitar) => {
+//   try {
+//     const token = getToken();
+//     const res = await fetch(`${api}/Guitar/${id}`, {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify(guitar),
+//     });
+//     if (!res.ok) {
+//       throw new Error(`Error update! Status: ${res.status}`);
+//     }
+//     return await res.json();
+//   } catch (err) {
+//     console.error("Update guitar error:", err);
+//   }
+// };
+
+// D
+// export const deleteGuitar = async (id) => {
+//   try {
+//     const token = getToken();
+//     const res = await fetch(`${api}/Guitar/${id}`, {
+//       method: "DELETE",
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     if (!res.ok) {
+//       throw new Error(`Error delete! Status: ${res.status}`);
+//     }
+//     return true;
+//   } catch (err) {
+//     console.error("Delete guitar error:", err);
+//   }
+// };
